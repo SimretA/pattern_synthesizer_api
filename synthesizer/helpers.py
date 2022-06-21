9fa7b755aad65c2419a71cc45ad7d9bac7b4266c
xiaohang-tang <52322566+xiaohang-tang@users.noreply.github.com>
2022-06-21 23:15:51 +0100

    Update helpers.py

--- synthesizer/helpers.py
+++ synthesizer/helpers.py
@@ -1,12 +1,17 @@
 from base64 import encode
 import spacy
+import copy
 from spacy.matcher import Matcher
 import hashlib
 import json
 import numpy as np
 from sklearn.metrics import precision_recall_fscore_support
+import pickle
 
+import gensim
+import gensim.downloader
 
+pretrained_vectors = gensim.downloader.load('word2vec-google-news-300')
 nlp = spacy.load("en_core_web_sm")
 
 def get_patterns(df, labels, chosenpatterns=None):
@@ -85,6 +90,7 @@ def expand_working_list(pat):
                 else:
                     for i in range(len(working_list)):
                         working_list[i].append(temp)
+        # print("pat: {} \n working_list {}".format(pat,working_list))
         return working_list
 
 
@@ -118,6 +124,99 @@ def match_positives(working_list, positive_examples, negative_set=False):
         match_count = matched_sentences
     return match_count
 
+def find_similar_words(lemmas, examples, threshold, topk_on=False, topk=1, negative_set=False):
+    # try:
+    #     with open('cache/similar_words_negative_set{}_examplenum_{}_threshold_{}_topkon_{}_topk_{}.pkl'.format(negative_set,len(examples),threshold,topk_on,topk), 'rb') as f:
+    #         similar_words = pickle.load(f)
+    try:
+        with open('cache/similar_words_examplenum_{}_threshold_{}_topkon_{}_topk_{}.pkl'.format(len(examples),threshold,topk_on,topk), 'rb') as f:
+            similar_words = pickle.load(f)
+    except FileNotFoundError:
+        similar_words = dict()
+    alreadyIn = set()
+    for lemma in lemmas:
+        if lemma in similar_words:
+            alreadyIn.add(lemma)
+            continue
+        print("new lemma")
+        similar_words[lemma] = dict()
+    if len(alreadyIn) == len(lemmas):
+        return similar_words
+    for lemma in lemmas:
+        if lemma in alreadyIn: continue
+        for _i, ex in enumerate(examples):
+            doc = nlp(str(ex))
+            for token in doc:
+                #get similarity
+                if not str(token) in similar_words[lemma] and not str(token.lemma_) in lemmas and not token.is_stop and not token.is_punct and len(token.text.strip(" "))>1:
+                    # similar_words[lemma][token.lemma_] = token.similarity(nlp(lemma))
+                    if token.lemma_ in pretrained_vectors and lemma in pretrained_vectors:
+                        similar_words[lemma][token.lemma_] = pretrained_vectors.similarity(token.lemma_,lemma)
+    for lemma in lemmas:
+        if lemma in alreadyIn: continue
+        if not topk_on:
+            similar_words[lemma] = {k:v for k,v in similar_words[lemma].items() if v>threshold}
+        else:
+            similar_words[lemma] = {k:v for k,v in sorted(similar_words[lemma].items(), key=lambda item: item[1], reverse=True)[:topk]}
+            similar_words[lemma] = similar_words[lemma]
+    # with open('cache/similar_words_negative_set{}_examplenum_{}_threshold_{}_topkon_{}_topk_{}.pkl'.format(negative_set,len(examples),threshold,topk_on,topk), 'wb') as f:
+    #     pickle.dump(similar_words,f)
+    with open('cache/similar_words_examplenum_{}_threshold_{}_topkon_{}_topk_{}.pkl'.format(len(examples),threshold,topk_on,topk), 'wb') as f:
+        pickle.dump(similar_words,f)
+    return similar_words
+
+def soft_match_positives(working_list, positive_examples, negative_set=False, price=None, threshold=0.5, topk_on=False, topk=1):
+    if (positive_examples == None or len(positive_examples) == 0):
+        return 0
+    match_count = 0
+    matched_sentences = 0
+    matcher = Matcher(nlp.vocab)
+    lemmas = []
+    for index, distinct_pattern in enumerate(working_list):
+        for pattern in distinct_pattern:
+            if 'LEMMA' in pattern and 'IN' in pattern['LEMMA'] and pattern['OP'] == '+':
+                lemmas += pattern['LEMMA']['IN']
+        
+    lemmas = list(set(lemmas))
+    if len(lemmas) > 0:
+        # print('start find' + str(len(positive_examples)))
+        similar_words = find_similar_words(lemmas, price["example"].values, threshold,negative_set=negative_set,topk_on=topk_on,topk=topk)
+        # print(similar_words)
+        for lemma in lemmas:
+            similar_words[lemma] = [k for k,v in similar_words[lemma].items()]
+        for index, distinct_pattern in enumerate(working_list):
+            for pattern in distinct_pattern:
+                if 'LEMMA' in pattern and 'IN' in pattern['LEMMA'] and pattern['OP'] == '+':
+                    if len(pattern['LEMMA']['IN']) <= 1 and pattern['LEMMA']['IN'][0] in similar_words:
+                        pattern['LEMMA']['IN'] += similar_words[pattern['LEMMA']['IN'][0]]
+                # print(pattern)
+    #         matcher.add(f"Posmatch{index}", [distinct_pattern])
+        
+    # else:
+    #     # print("no LITERAS")
+    #     for index, distinct_pattern in enumerate(working_list):
+    #         matcher.add(f"Posmatch{index}", [distinct_pattern])
+    # match_collector = dict()
+    # for _i, doc in enumerate(positive_examples):
+    #     matched = False
+    #     matches = matcher(doc)
+    #     if (matches is not None and len(matches) > 0):
+    #         for id, start, end in matches:
+    #             rule = id
+    #             if (str(doc[start:end]).strip() != ""):
+    #                 matched = True
+    #                 if (rule in match_collector):
+    #                     match_collector[rule].append((start, end))
+    #                 else:
+    #                     match_collector[rule] = [(start, end)]
+
+    #                 # print(f'sent={_i} pat={[doc.vocab.strings[id]]}, mathcedspan={doc[start:end]}')
+    #     if (matched):
+    #         matched_sentences += 1
+    #         # print(f"{sent}, {match_collector}")
+    # if (len(set(match_collector.keys())) == len(working_list)) or negative_set:
+    #     match_count = matched_sentences
+    # return match_count
 
 def show_patters(patterns, out=None):
     results = sorted(patterns.keys(), key=len, reverse=True)
